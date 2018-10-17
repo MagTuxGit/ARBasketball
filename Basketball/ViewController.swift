caf0f49033e8bd5fe15e05d56033ee573902084f
Andriy Trubchanin <magtuxgit@gmail.com>
2018-10-17 12:08:43 +0300

    action (round moving)

--- Basketball/ViewController.swift
+++ Basketball/ViewController.swift
@@ -65,13 +65,24 @@ class ViewController: UIViewController {
 
         sceneView.scene.rootNode.addChildNode(backboardNode)
         
-        horizontalAction(node: backboardNode)
+        //horizontalAction(node: backboardNode)
+        roundAction(node: backboardNode)
     }
     
     func horizontalAction(node: SCNNode) {
         let leftAction = SCNAction.move(by: SCNVector3(x: -1, y: 0, z: 0), duration: 3)
         let rightAction = SCNAction.move(by: SCNVector3(x: 1, y: 0, z: 0), duration: 3)
         let actionSequence = SCNAction.sequence([leftAction, rightAction])
+        node.runAction(SCNAction.repeat(actionSequence, count: 2))
+    }
+    
+    func roundAction(node: SCNNode) {
+        let upLeft = SCNAction.move(by: SCNVector3(x: 1, y: 1, z: 0), duration: 2)
+        let downLeft = SCNAction.move(by: SCNVector3(x: 1, y: -1, z: 0), duration: 2)
+        let downRight = SCNAction.move(by: SCNVector3(x: -1, y: -1, z: 0), duration: 2)
+        let upRight = SCNAction.move(by: SCNVector3(x: -1, y: 1, z: 0), duration: 2)
+
+        let actionSequence = SCNAction.sequence([upLeft, downLeft, downRight, upRight])
         node.runAction(SCNAction.repeat(actionSequence, count: 2))
     }
     
